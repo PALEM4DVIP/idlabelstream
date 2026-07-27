80929e600f49af9e1717e124b01115e389280907
PALEM4DVIP <aditcanva01@gmail.com>
2026-07-27 16:31:32 +0700

    Add files via upload

--- api/stream/[id].js
+++ api/stream/[id].js
@@ -1,1 +1,11 @@
+const { matches } = require('../_data');
 
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { id } = req.query;
+  const match = matches.find(m => m.id === id);
+  if (!match) {
+    return res.status(404).json({ message: 'Pertandingan tidak ditemukan' });
+  }
+  res.status(200).json(match);
+};
